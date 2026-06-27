f90a31b308de347165871b7b118a42b74ba3a4be
tarun <tonystark142333@gmail.com>
2026-06-27 21:12:17 +0530

    best v1

--- TF/upload.sh
+++ TF/upload.sh
@@ -668,4 +668,4 @@ done
 
 echo -e "\n${BLD}${GRN}════════ All Batches Complete ════════${NC}"
 echo -e "${BLU}Log saved to: $LOGFILE${NC}"
-log "===== Session completed ======"
+log "======== Session completed and Upload Done ========="
